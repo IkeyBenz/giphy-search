445d7b401c233622aafef68b23e32d74a069f648
Ikey Benzaken <ikey.benz@gmail.com>
2018-08-30 13:32:42 -0700

    Initial Commit and final project

---
+++ index.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let hbs = require('express-handlebars');
+let giphy = require('giphy-api')();
+let app = express();
+
+app.engine('handlebars', hbs());
+app.use(express.static('public'));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    let searchTerm = req.query.term || "funny cat";
+    giphy.search(searchTerm, (err, response) => {
+        res.render('home', {gifs: response.data});
+    });
+});
+
+app.listen(5000, console.log("Listening on port 5000"));
+
